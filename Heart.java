5f5466933b805ea8bb73bd027f59f5a2d8c54002
fionahoo <101162000+fionahoo@users.noreply.github.com>
2022-05-30 10:55:21 -0400

    added heart class

---
+++ Heart.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Heart here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Heart extends Actor
+{
+    private GreenfootImage fullHeart = new GreenfootImage("full-heart.png");
+    private GreenfootImage emptyHeart = new GreenfootImage("empty-heart.png");
+    MyWorld world = (MyWorld) getWorld();
+    
+    /**
+     * Act - do whatever the Heart wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
